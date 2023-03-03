f0b36be09b5a77dc2f7400c4e33fbeaec9314c95
James Milligan <james@omnant.co.uk>
2023-03-03 13:26:33 +0000

    bump
    Signed-off-by: James Milligan <james@omnant.co.uk>

--- flagd/cmd/doc.go
+++ flagd/cmd/doc.go
@@ -2,7 +2,7 @@ package cmd
 
 import "github.com/spf13/cobra/doc"
 
-// GenerateDoc generates cobra docs of the cmd
+// GenerateDoc generates cobra docs of the cmd bump
 func GenerateDoc(path string) error {
 	return doc.GenMarkdownTree(rootCmd, path)
 }
