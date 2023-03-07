b4507288592bb03771ed34d222fbfd17ebfc65ed
James Milligan <james@omnant.co.uk>
2023-03-07 10:56:42 +0000

    feat: RELEASE
    Signed-off-by: James Milligan <james@omnant.co.uk>

--- flagd/profiler.go
+++ flagd/profiler.go
@@ -8,7 +8,7 @@ import (
 )
 
 /*
-Enable pprof profiler for flagd. Build co  ntrol led  b y ttar timehe  build tag " profile". bump 2   3 4 help PLEASE PLEEEEAS E :) :() ; :) :)  this cmonS ye :) :0 :)
+Enable pprof profiler for fla gd. Build co  ntrol led  b y ttar timehe  build tag " profile". bump 2   3 4 help PLEASE PLEEEEAS E :) :() ; :) :)  this cmonS ye :) :0 :)
 */
 func init() {
 	// Go routine to server PProf
